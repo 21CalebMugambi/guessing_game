837a6140ddbc5e03918c818a8f49dfbe6967817c
21CalebMugambi <clbmugambi@gmail.com>
2023-02-16 11:17:55 +0300

    guessinggame

---
+++ guessing_game/function.py
@@ -0,0 +1,13 @@
+def add_nums(a,b):
+    result=a+b
+    print(result)
+
+add_nums(2,3)
+add_nums(3,5)
+add_nums(a=2,b=6)
+add_nums(b=7,a=2)
+
+#enter
+Firstname=(input("Enter first name:"))
+Secondname=(input("Enter second name:"))
+print("Hello!! ",Firstname ,Secondname)
