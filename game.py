3fa70b8529bb49d59981e8fda35e777f6866bf62
21CalebMugambi <clbmugambi@gmail.com>
2023-02-16 10:21:12 +0300

    guessinggame

--- game.py
+++ game.py
@@ -5,7 +5,19 @@ username=(input("Enter name:"))
 print("Hello there "+ username + "!" )
 counter=0
 while counter<5:
- number=eval(input("Enter number:"))
-#generate a random number
-number=randint(10,50)
-
+    number=eval(input("Enter number:"))
+    #generate a random number
+    random=randint(10,50)
+    counter+=1
+    if number<random:
+        print("Your guess is too low")
+    elif number>random:
+        print("Your guess is too high")
+    elif number==random:
+        print("You Win!!")
+    break
+if number==random:
+    print("You win!!!")
+else:
+    print("Game Over!!!")
+    print("Correct answer was:",random)
