30ecf90fa32835454c57cbb0f933be67437a8a6b
21CalebMugambi <clbmugambi@gmail.com>
2023-02-16 09:25:01 +0300

    guessinggame

---
+++ game.py
@@ -0,0 +1,11 @@
+
+from random import randint
+#get user input
+username=(input("Enter name:"))
+print("Hello there "+ username + "!" )
+counter=0
+while counter<5:
+ number=eval(input("Enter number:"))
+#generate a random number
+number=randint(10,50)
+
